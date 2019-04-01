35478a2477604d3568ff89379b4d0c9c39aaf4da
Mathieu Pont <matthieu.pont@hotmail.fr>
2019-04-01 20:10:38 +0200

    finalize game environment display

--- src/escape_environment/Environment.java
+++ src/escape_environment/Environment.java
@@ -5,8 +5,8 @@ public class Environment {
 	boolean use_FX_Viewer;
 	Viewer viewer;
 	
-	int[] coord_actuator;
-	int[] coord_runner;
+	int[] coord_actuator = new int[2];
+	int[] coord_runner = new int[2];
 	
 	int runner_room;
 	int switch_on;
@@ -18,16 +18,17 @@ public class Environment {
 		else
 			this.viewer = new SimpleViewer();
 		
-		/*this.coord_actuator[0] = 0;
-		this.coord_actuator[0] = 0;*/
+		this.coord_actuator[0] = 3;
+		this.coord_actuator[1] = 3;
+		
+		this.coord_runner[0] = 2;
+		this.coord_runner[1] = 9;
 		
 		this.runner_room = 0;
 		this.switch_on = -1;
 	}
 	
 	public void display() {
-		this.viewer.display(coord_actuator, coord_runner, runner_room, switch_on);
-		this.runner_room = 1;
 		this.viewer.display(coord_actuator, coord_runner, runner_room, switch_on);
 	}
 }
--- src/escape_environment/SimpleViewer.java
+++ src/escape_environment/SimpleViewer.java
@@ -29,49 +29,46 @@ public class SimpleViewer implements Viewer {
     	for(int i = 0 ; i < num_row; ++i) {
 			lines[i] = "";
     		if(i == 0) {
-    			lines[i] += wall+wall+wall+wall+wall+wall;
+    			lines[i] += wall+wall+wall+wall+wall+wall+wall;
 				lines[i] += empty;
     			if(runner_room == 1)
     				lines[i] += empty+empty+empty;
     			lines[i] += wall+wall+wall+wall;
     		}else if(i == 1) {
     			lines[i] += wall;
-    			for(int j = 0 ; j < 4 ; ++j)
+    			for(int j = 0 ; j < 5 ; ++j)
     				lines[i] += (j == switch_on_id) ? switch_on : switch_off;
     			lines[i] += wall;
     			lines[i] += empty;
-    			if(runner_room == 0) {
+    			if(runner_room == 0) 
     				lines[i] += wall+empty+empty+door;	
-    			} 
-    			else {
+    			else 
     				lines[i] += empty+empty+empty+door+empty+empty+wall;
-    			}
     		}else if(i == 2) {
-    			lines[i] += wall+empty+empty+empty+empty+wall;
+    			lines[i] += wall+empty+empty+empty+empty+empty+wall;
     			lines[i] += empty;
-    			if(runner_room == 0) {
+    			if(runner_room == 0) 
     				lines[i] += wall+empty+empty+code;	
-    			} 
-    			else {
+    			else 
     				lines[i] += empty+empty+empty+code+empty+empty+wall;
-    			}
     		}else if(i == 3) {
-    			lines[i] += wall+empty+empty+empty+empty+wall;
+    			lines[i] += wall+empty+empty+empty+empty+empty+wall;
     			lines[i] += empty; 
-    			if(runner_room == 0) {
+    			if(runner_room == 0) 
     				lines[i] += wall+empty+empty+door;	
-    			} 
-    			else {
+    			else 
     				lines[i] += empty+empty+empty+door+empty+empty+wall;
-    			}
     		}else if(i == 4) {
-    			lines[i] += wall+wall+wall+wall+wall+wall;
+    			lines[i] += wall+wall+wall+wall+wall+wall+wall;
 				lines[i] += empty;
     			if(runner_room == 1)
     				lines[i] += empty+empty+empty;
     			lines[i] += wall+wall+wall+wall;
     		}
     	}
+    	
+    	lines[coord_actuator[0]] = lines[coord_actuator[0]].substring(0,coord_actuator[1])+agent+lines[coord_actuator[0]].substring(coord_actuator[1]+1);
+    	lines[coord_runner[0]] = lines[coord_runner[0]].substring(0,coord_runner[1])+agent+lines[coord_runner[0]].substring(coord_runner[1]+1);
     	
     	for(int i = 0 ; i < num_row; ++i) {
     		System.out.println(lines[i]);
